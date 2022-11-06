ddbd75674723b469af45a707de76c0c2e01c354b
afize1 <115039188+afize1@users.noreply.github.com>
2022-11-07 00:27:01 +0300

    First commit of the i-hub project
    -Signup and login process have been done.
    -Forgot password structure have been created. Getting the security question object should be arranged.
    Related task ids:9,10,11,12

---
+++ iHub/project/key.py
@@ -0,0 +1,5 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+# The Django secret key is used to provide cryptographic signing. 
+# This key is mostly used to sign session cookies.
+# If one were to have this key, they would be able to modify the cookies sent by the application.
+SECRET_KEY = 'nrej2jp$2d68126uzag@$3(+&54!#fo2^s@yf_e$)ij%0e&7gv'
