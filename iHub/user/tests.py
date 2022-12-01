621f71578db2fa914e1755490599b863a7febd2a
afize1 <115039188+afize1@users.noreply.github.com>
2022-12-01 23:23:53 +0300

    signup, login and forgot password unit tests are implemented
    1)signup,
    2)login for both correct credentials and wrong credentials
    3)forgot pasword unit tests are implemented.

--- iHub/user/tests.py
+++ iHub/user/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate,login
+from django.contrib.auth.models import User
+from .models import users
 
-# Create your tests here.
+
+
+class createUserTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="test",first_name="testname", last_name="testlastname", email="test@test.com",password="1234Test")
+        self.user = users.objects.create(username="test", answer="test12", security_question="1" )
+        self.user.save()
+
+    def test_loginTest(self):
+        user = authenticate(username='test', password='1234Test')
+        self.assertTrue((user is not None) and user.is_authenticated)
+
+    def test_loginWrongName(self):
+        user = authenticate(username='test12', password='1234Test')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_LoginWrongPassword(self):
+        user = authenticate(username='test', password='1234Test1234')
+        self.assertFalse(user is not None and user.is_authenticated)
+
+    def test_forgotPassword(self):
+        user = users.objects.filter(username="test")
+        self.assertEqual(user[0].security_question, "1")
+        self.assertEqual(user[0].answer, "test12")
+
+
+
+
