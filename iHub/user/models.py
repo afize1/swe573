e2c59479a91e915527c8bdaba08d2f6b0fef82b0
afize1 <115039188+afize1@users.noreply.github.com>
2022-12-26 01:42:46 +0300

    Frontend template is changed
    -frontend template has been changed
    -while following&follower structure was tried to fix, encountered with db problems. To solve it, migrations were deleted but after that db was broken totally. To fix all the problems ,migration files was returned back to the cache, and migration statements were run.

--- iHub/user/models.py
+++ iHub/user/models.py
@@ -8,45 +8,47 @@ DEMO_CHOICES =(
     ("3", "What is your favorite song name"),
 )
 
-
-class shares(models.Model):
-    username=models.CharField(max_length=20)
-    subject= models.CharField(max_length=255)
-    label = models.CharField(max_length=255)
-    private = models.CharField(max_length=20)
-    value=models.CharField(max_length=255)
-    comment = models.CharField(max_length=255)
-    class Meta:
-        db_table='db_shares'
-        
-class followUser(models.Model):
-    username=models.CharField(max_length=20)
-    followers= models.CharField(max_length=20)
-    class Meta:
-        db_table='db_followers'
-
 class users(models.Model):
-    username= models.CharField(max_length=20)
-    first_name= models.CharField(max_length=20)
-    last_name= models.CharField(max_length=20)
-    security_question = models.CharField(max_length=100)
-    answer = models.CharField(max_length=20)
+    username= models.CharField(max_length=255)
+    first_name= models.CharField(max_length=255)
+    last_name= models.CharField(max_length=255)
+    security_question = models.CharField(max_length=255)
+    answer = models.CharField(max_length=255)
     image = models.ImageField(upload_to="images/")
-    occupation= models.CharField(max_length=20)
-    interest= models.CharField(max_length=20)
+    occupation= models.CharField(max_length=255)
+    interest= models.CharField(max_length=255)
     bio= models.CharField(max_length=255)
+    followings = models.IntegerField()
+    followers = models.IntegerField()
     class Meta:
         db_table='db_users'
         
 class images(models.Model):
-    username= models.CharField(max_length=20)
+    username= models.CharField(max_length=255)
     image = models.ImageField(upload_to="images/")
     class Meta:
         db_table='db_images'
 
+class shares(models.Model):
+    username=models.CharField(max_length=255)
+    subject= models.CharField(max_length=255)
+    label = models.CharField(max_length=255)
+    private = models.CharField(max_length=255)
+    type = models.CharField(max_length=20)
+    content=models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    class Meta:
+        db_table='db_shares'
+        
+class followUser(models.Model):
+    username=models.CharField(max_length=255)
+    followers= models.CharField(max_length=255)
+    class Meta:
+        db_table='db_followers'
+        
 class postComment(models.Model):
     post_id=models.IntegerField();
-    username=models.CharField(max_length=20)
+    username=models.CharField(max_length=255)
     comment= models.CharField(max_length=255)
     class Meta:
         db_table='db_post_comment'
