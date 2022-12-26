2fc09a70fe7808bc557efb3c35aee5112ff886c5
afize1 <115039188+afize1@users.noreply.github.com>
2022-12-26 23:12:19 +0300

    Errors are fixed after user tests
    -following and follower numbers could not updated. It is fixed!
    -Date was added to the posts.
    -Create post method was added for unit tests.
    -Dockerization problem has been solved. PIL library has been added to the requirements.txt and run pillow command to the docker file.

--- iHub/user/tests.py
+++ iHub/user/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import authenticate,login
 from django.contrib.auth.models import User
-from .models import users
+from .models import users,shares
 
 
 
@@ -9,6 +9,7 @@ class createUserTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="test",first_name="testname", last_name="testlastname", email="test@test.com",password="1234Test")
         self.user = users.objects.create(username="test", answer="test12", security_question="1" )
+        self.user= shares.objects.create( username="test",subject= "django",label = "development",private = "1",type = "1", content="text", description = "nice test")
         self.user.save()
 
     def test_loginTest(self):
@@ -27,6 +28,16 @@ class createUserTests(TestCase):
         user = users.objects.filter(username="test")
         self.assertEqual(user[0].security_question, "1")
         self.assertEqual(user[0].answer, "test12")
+
+    def test_createPost(self):
+        user = users.objects.filter(username="test")
+        self.assertEqual(user[0].subject, "django")
+        self.assertEqual(user[0].label, "development")
+        self.assertEqual(user[0].private, "1")
+        self.assertEqual(user[0].type, "1")
+        self.assertEqual(user[0].content, "text")
+        self.assertEqual(user[0].description, "nice test")
+
 
 
 
